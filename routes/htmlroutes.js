5c0e9f945f81722c710c191a08251a06efedae27
Philip Garip <phil.garip@gmail.com>
2021-06-08 14:05:02 -0400

    clean up

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,8 +1,6 @@
 const path = require("path");
 
 module.exports = function(app) {
-
-    
   
     app.get("/exercise", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/exercise.html"));
