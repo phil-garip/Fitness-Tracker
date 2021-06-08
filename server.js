2c26930f3043b40c30d3fd4606fc95e2ef87c556
Philip Garip <phil.garip@gmail.com>
2021-06-07 20:57:57 -0400

    Added mongoose DB connection

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongojs = require("mongojs");
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl
